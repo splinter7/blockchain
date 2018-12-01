be5901b0aa00e7d3bc9486fc148fc5705a658ea9
Splinter7 <stewart@splinter7.com>
2018-12-01 15:10:01 -0500

    Add interfacebetween wallet and transaction pool

--- wallet/index.js
+++ wallet/index.js
@@ -1,21 +1,40 @@
-const {INITIAL_BALANCE} = require('../config')
-const ChainUtil = require('../chain-util')
+const { INITIAL_BALANCE } = require("../config");
+const Transaction = require("./transaction");
+const ChainUtil = require("../chain-util");
 
 class Wallet {
-  constructor(){
+  constructor() {
     this.balance = INITIAL_BALANCE;
     this.keyPair = ChainUtil.genKeyPair();
-    this.publicKey = this.keyPair.getPublic().encode('hex');
+    this.publicKey = this.keyPair.getPublic().encode("hex");
   }
 
-  toString(){
+  toString() {
     return `Wallet -
     Public Key: ${this.publicKey.toString()}
-    Balance: ${this.balance}`
+    Balance: ${this.balance}`;
   }
 
   sign(dataHash) {
     return this.keyPair.sign(dataHash);
+  }
+
+  createTransaction(recipient, amount, transactionPool) {
+    if (amount > this.balance) {
+      console.log(`Amount: ${amount} exceeds current balance ${this.balance}`);
+      return;
+    }
+
+    let transaction = transactionPool.existingTransaction(this.publicKey);
+
+    if (transaction) {
+      transaction.update(this, recipient, amount);
+    } else {
+      transaction = Transaction.newTransaction(this, recipient, amount);
+      transactionPool.updateOrAddTransaction(transaction);
+    }
+
+    return transaction;
   }
 }
 
