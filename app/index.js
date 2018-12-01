dc8c86ddce16b81ed12cb880be12588f89b89fa6
Splinter7 <stewart@splinter7.com>
2018-12-01 15:49:13 -0500

    Added transaction endpoints

--- app/index.js
+++ app/index.js
@@ -1,27 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const Blockchain = require('../blockchian');
-const P2pServer = require('./p2p-server')
+const express = require("express");
+const bodyParser = require("body-parser");
+const Blockchain = require("../blockchian");
+const P2pServer = require("./p2p-server");
+const Wallet = require("../wallet");
+const TransactionPool = require("../wallet/transaction-pool");
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
-app.get('/blocks', (req, res) => {
+app.get("/blocks", (req, res) => {
   res.json(bc.chain);
 });
 
-app.post('/mine', (req, res) => {
+app.post("/mine", (req, res) => {
   const block = bc.addBlock(req.body.data);
   console.log(`New block added: ${block.toString()}`);
 
   p2pServer.synChains();
 
-  res.redirect('/blocks');
+  res.redirect("/blocks");
+});
+
+app.get("/transactions", (req, res) => {
+  res.json(tp.transactions);
+});
+
+app.post("/transact", (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  res.redirect("/transactions");
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
